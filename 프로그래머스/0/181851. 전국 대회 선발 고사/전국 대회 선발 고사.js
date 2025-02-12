554646d03f8329a002a637a4fdbcf1811d81af02
박성택 <55645972+stoic-park@users.noreply.github.com>
2025-02-12 22:53:16 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.38 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -2,11 +2,18 @@
 // 2. 정렬
 // 3. 인덱스값 찾아서 계산
 function solution(rank, attendance) {
-    const target = rank.filter((a, index) => attendance[index] === true).sort((a,b) => a - b);
+//     const target = rank.filter((a, index) => attendance[index] === true).sort((a,b) => a - b);
 
-    const rank1 = rank.indexOf(target[0]);
-    const rank2 = rank.indexOf(target[1]);
-    const rank3 = rank.indexOf(target[2]);
+//     const rank1 = rank.indexOf(target[0]);
+//     const rank2 = rank.indexOf(target[1]);
+//     const rank3 = rank.indexOf(target[2]);
     
-    return (10000*rank1) + (100*rank2) + rank3;
+//     return (10000*rank1) + (100*rank2) + rank3;
+
+    const [a,b,c] = rank.map((r, i) => [r, i])
+    .filter(([_, i]) => attendance[i])
+    .sort(([a], [b]) => a - b);
+
+    return 10000 * a[1] + 100 * b[1] + c[1];
+    
 }
