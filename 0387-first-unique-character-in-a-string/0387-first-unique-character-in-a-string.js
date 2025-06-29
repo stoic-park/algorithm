caa2de4d042b4a5981090707f1dd45bc72c642ac
박성택 <55645972+stoic-park@users.noreply.github.com>
2025-06-30 00:39:39 +0900

    Time: 44 ms (12.25%), Space: 60.8 MB (30.91%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -7,13 +7,34 @@ var firstUniqChar = function(s) {
     // indexOf, lastIndexOf를 이용
     // 아무래도 이 방법은 아닌 것 같은데
     
-    let result = -1;
+    // let result = -1;
 
-    for (let i = 0; i < s.length; i++) {
-        if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
-            result = i;
-            break;
-        }
+    // for (let i = 0; i < s.length; i++) {
+    //     if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+    //         result = i;
+    //         break;
+    //     }
+    // }
+    // return result;
+
+
+  const count = new Map();
+
+  // 1) 각 문자별로 카운트
+  for (let char of s) {
+    if (count.has(char)) {
+        count.set(char, count.get(char) + 1);
+    } else {
+        count.set(char, 1);
     }
-    return result;
+  }
+
+  // 2) 순서대로 보면서 카운트가 1인 첫 문자 찾기
+  for (let i=0; i<s.length; i++) {
+    if (count.get(s[i]) === 1) {
+      return i;
+    }
+  }
+
+  return -1;
 };
